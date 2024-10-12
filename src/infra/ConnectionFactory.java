b1db2b3d3aa9e2d8756136d99e5a1fcc4b98be60
Leo B <lszborges@gmail.com>
2024-10-12 10:11:30 -0300

    Changed connection file

--- src/infra/ConnectionFactory.java
+++ src/infra/ConnectionFactory.java
@@ -18,10 +18,10 @@ public class ConnectionFactory {
             Properties properties = new Properties();
             properties.load(ConnectionFactory.class.getResourceAsStream("/resources/connection.properties"));
             
-            String dbUrl = properties.getProperty("db.url");
-            String dbUser = properties.getProperty("db.user");
-            String dbPass = properties.getProperty("db.password");
-            
+            String dbUrl = System.getenv("DB_URL") != null ? System.getenv("DB_URL") : properties.getProperty("db.url");
+            String dbUser = System.getenv("DB_USER") != null ? System.getenv("DB_USER") : properties.getProperty("db.user");
+            String dbPass = System.getenv("DB_PASSWORD") != null ? System.getenv("DB_PASSWORD") : properties.getProperty("db.password");
+            System.out.println("DB: " + dbUrl);
             return DriverManager.getConnection(dbUrl, dbUser, dbPass);
         } catch (Exception e) {
             throw new RuntimeException(e);
